980d532dadbee43ca5d655c0c0b6e7e8f69867c9
ACola03 <166641004+ACola03@users.noreply.github.com>
2026-05-31 01:02:09 -0400

    updated test cases

--- jdTest.R
+++ jdTest.R
@@ -9,9 +9,14 @@ library(shellpipes)
 
 source("trianglePlot_Poisson.R")
 
-## trianglePlot(lambda = c(1,3,5), plot = "both", testv = "poisson.test", fuzz.x = TRUE, add.checkplot = TRUE)
 trianglePlot(lambda = c(1,3,5), plot = "both", testv = "poisson.test", fuzz.x = FALSE, add.checkplot = TRUE)
-trianglePlot(lambda = c(1,3, 5), plot = "one", testv = "wald.intercept", fuzz.x = FALSE, add.checkplot = TRUE)
+
+trianglePlot(lambda = c(1,3,5), plot = "both", testv = "wald.intercept", fuzz.x = FALSE, add.checkplot = TRUE)
+trianglePlot(lambda = c(1,3,5), plot = "both",  numReps = 10, testv = "wald.intercept", fuzz.x = FALSE, add.checkplot = TRUE)
+trianglePlot(lambda = c(1,3,5), plot = "both",  numReps = 100, testv = "wald.intercept", fuzz.x = FALSE, add.checkplot = TRUE)
+
+dat <- rpois(1e4, lambda = 3)
+trianglePlot(lambda = c(1,3,5), dat = dat, plot = "both", testv = "poisson.test", fuzz.x = FALSE, add.checkplot = TRUE)
 
 
 
